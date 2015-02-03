097dd714eb3bd8623984863107a6a75c1778453e
go2 <georgizlatkov@yahoo.com>
2015-02-03 22:36:31 +0000

    Quick unverified commit before I start working again

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 StockScreens
 ============
+
+A hobby project. 
--- stock_pull.py
+++ stock_pull.py
@@ -9,17 +9,37 @@ import time
 import csv
 import os
 import sys
+import sqlite3
 
 from datetime import datetime
 
-
-stocks = ['GOOG', 'AAPL', 'MSFT', 'AMZN', 'EBAY', 'TSLA']
 directory = 'data/'
 
 url = 'http://chartapi.finance.yahoo.com/instrument/1.0/%s/chartdata;type=quote;range=%s/csv/'
 
 
-def pull_data(stock):
+def init_db(db_name):
+        """
+            Connect to database and create the necessary tables if they don't exist
+        """
+
+        conn = sqlite3.connect(db_name)
+        cur = conn.cursor()
+
+        # Ugly but quick way to apply simple changes to the tables
+        # cur.execute('''DROP TABLE tDayStock''')
+        # cur.execute('''DROP TABLE tMinuteStock''')
+        # conn.commit()
+
+        # Create tables if they don't exist
+        cur.execute('''CREATE TABLE IF NOT EXISTS tDayStock (ticker TEXT, datestamp TEXT,
+                        high REAL, low REAL, open REAL, close REAL, volume REAL, PRIMARY KEY (ticker, datestamp))''')
+        cur.execute('''CREATE TABLE IF NOT EXISTS tMinuteStock (ticker TEXT, datestamp TEXT,
+                        high REAL, low REAL, open REAL, close REAL, volume REAL, PRIMARY KEY (ticker, datestamp))''')
+        return conn
+
+
+def get_data_yahoo_year(stock):
     try:
         print(str(time.strftime('%Y-%m-%d %H:%M:%S')))
         print('Pulling: ' + stock)
@@ -29,6 +49,22 @@ def pull_data(stock):
 
         source = urllib2.urlopen(url_stock)
         cr = list(csv.reader(source))
+
+        cur = database.cursor()
+        cur.execute('SELECT * FROM tDayStock')
+        data = cur.fetchone()
+
+        data = list()
+        for row in cr:
+            if len(row) == 6 and 'values' not in row[0]:
+                # if int(row[0]) > int(last):
+                row.insert(0, stock)
+                data.append(tuple(row))
+
+        cur.executemany('''INSERT INTO tDayStock(ticker, datestamp, high, low, open, close, volume)
+                        VALUES(?, ?, ?, ?, ?, ?, ?)''', data)
+
+        return 0
 
         try:
             with open(filename) as f:
@@ -48,7 +84,7 @@ def pull_data(stock):
             f.write(data.strip('\n'))
 
         print('Pulled: ' + stock)
-        time.sleep(5)
+        time.sleep(0)
 
     except Exception, e:
         print('Problem: %s' % e)
@@ -60,10 +96,13 @@ def main():
         os.makedirs(directory)
 
     while True:
-        for company in stocks:
-            pull_data(company)
-        print('Sleeping... \n')
-        time.sleep(300)
+        for company in const._stocks:
+            get_data_yahoo_year(company)
+        break
+
+    database.commit()
+    database.close()
 
 if __name__ == '__main__':
+    database = init_db('finance_data.db')
     main()
